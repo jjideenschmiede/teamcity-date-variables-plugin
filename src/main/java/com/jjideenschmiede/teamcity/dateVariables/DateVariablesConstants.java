af6cc852831f395895b19171c8372f7cedef7658
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2025-04-22 16:31:42 +0200

    Initial commit

---
+++ src/main/java/com/jjideenschmiede/teamcity/dateVariables/DateVariablesConstants.java
@@ -0,0 +1,17 @@
+package de.teamcity.datevariables;
+
+/**
+ * Constants for date variables plugin
+ */
+public final class DateVariablesConstants {
+	public static final String PREFIX = "teamcity.date.";
+
+	public static final String YEAR = PREFIX + "year";
+	public static final String MONTH = PREFIX + "month";
+	public static final String DAY = PREFIX + "day";
+	public static final String HOUR = PREFIX + "hour";
+	public static final String MINUTE = PREFIX + "minute";
+	public static final String SECOND = PREFIX + "second";
+	public static final String TIMESTAMP = PREFIX + "timestamp";
+	public static final String ISO_DATE = PREFIX + "iso";
+}
---
+++ src/main/java/com/jjideenschmiede/teamcity/dateVariables/DateVariablesProvider.java
@@ -0,0 +1,39 @@
+package de.teamcity.datevariables;
+
+import jetbrains.buildServer.serverSide.SBuild;
+import jetbrains.buildServer.serverSide.parameters.AbstractBuildParametersProvider;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Provides date-related parameters to TeamCity builds
+ */
+public class DateVariablesProvider extends AbstractBuildParametersProvider {
+
+	@NotNull
+	@Override
+	public Map<String, String> getParameters(@NotNull SBuild build, boolean emulationMode) {
+		Map<String, String> params = new HashMap<>();
+
+		ZonedDateTime now = ZonedDateTime.now(ZoneId.systemDefault());
+
+		params.put(DateVariablesConstants.YEAR, String.valueOf(now.getYear()));
+		params.put(DateVariablesConstants.MONTH, String.format("%02d", now.getMonthValue()));
+		params.put(DateVariablesConstants.DAY, String.format("%02d", now.getDayOfMonth()));
+		params.put(DateVariablesConstants.HOUR, String.format("%02d", now.getHour()));
+		params.put(DateVariablesConstants.MINUTE, String.format("%02d", now.getMinute()));
+		params.put(DateVariablesConstants.SECOND, String.format("%02d", now.getSecond()));
+
+		params.put(DateVariablesConstants.TIMESTAMP, String.valueOf(now.toEpochSecond()));
+
+		params.put(DateVariablesConstants.ISO_DATE, now.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
+
+		return params;
+	}
+}
